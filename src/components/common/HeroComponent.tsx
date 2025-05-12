bdd31d72cc20545424950e3a45c5c71cd8d14dd2
Pratham8487 <prathamshah8487@gmail.com>
2025-05-12 13:00:43 +0530

    Made HeroComponent resuable

--- src/components/common/HeroComponent.tsx
+++ src/components/common/HeroComponent.tsx
@@ -1,26 +1,27 @@
 import Button from "@mui/material/Button";
-import HomeImg from "../../assets/painting.svg";
 
-const HeroComponent = () => {
+interface HeroComponentProps {
+  img?: string;
+  description?: React.ReactNode;
+  heading?: React.ReactNode;
+}
+
+const HeroComponent = ({ img, heading, description }: HeroComponentProps) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 space-y-5 sm:space-y-5 md:space-y-0 md:space-x-10 lg:space-y-0 lg:space-x-10  p-4 sm:p-2 lg:p-4 md:p-4 ">
       <div className=" flex items-center justify-center ">
-        <img src={HomeImg} className="shadow-sky-950 h-76 w-76" />
+        <img src={img} className="shadow-sky-950 h-full w-full" />
       </div>
 
       <div className="flex flex-col gap-4 justify-center sm:pl-4 md:col-span-2 lg:col-span-2 ">
         <h1 className="text-2xl sm:text-2xl md:text-4xl lg:text-4xl font-semibold text-[#4D4D4D] text-balance text-center sm:text-left">
-          The unseen of spending three years at Pixelgrade
+          {heading}
         </h1>
 
         <h2 className="text-[#89939E] text-base text-balance text-justify sm:text-left">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-          ducimus quaerat repellat sit rerum doloremque aperiam ea optio modi
-          tempora iure illum quisquam perspiciatis quidem voluptates, fugiat,
-          voluptatibus, ex numquam. Debitis quo, modi dicta officia officiis
-          nihil illo quos saepe maiores voluptatem laboriosam eaque autem
-          obcaecati delectus reprehenderit similique reiciendis, voluptatibus
-          pariatur incidunt explicabo sequi? Officia amet qui sunt ratione!
+          {description
+            ? description
+            : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus quaerat repellat sit rerum doloremque aperiam ea optio modi tempora iure illum quisquam perspiciatis quidem voluptates, fugiat, voluptatibus, ex numquam. Debitis quo, modi dicta officia officiis nihil illo quos saepe maiores voluptatem laboriosam eaque autem obcaecati delectus reprehenderit similique reiciendis, voluptatibus pariatur incidunt explicabo sequi? Officia amet qui sunt ratione!"}
         </h2>
 
         <Button
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,12 @@ import BrandLogo from "../components/BrandLogo";
 import Card from "../components/common/Card";
 import HeroSection from "../components/common/HeroSection";
 import HeroComponent from "../components/common/HeroComponent";
+import HomeImg from "../assets/painting.svg";
+import Pana from "../assets/pana.svg";
+import Icon1 from "../assets/Icon-1.svg";
+import Icon2 from "../assets/Icon-2.svg";
+import Icon4 from "../assets/Icon-4.svg";
+import Icon3 from "../assets/Icon-3.svg";
 
 function HomePage() {
   return (
@@ -18,8 +24,7 @@ function HomePage() {
       <div className="gap-2 flex items-center justify-center flex-col p-6">
         <h1 className="text-2xl sm:text-2xl md:text-4xl lg:text-4xl font-semibold text-[#4D4D4D] text-center max-w-4xl mx-auto leading-tight">
           Manage your entire community
-          <br className="hidden md:block" />{" "}
-          in a single system
+          <br className="hidden md:block" /> in a single system
         </h1>
         <h2 className="text-[#89939E] text-base text-center">
           Who is Nextcent suitable for?
@@ -42,7 +47,88 @@ function HomePage() {
         membership renewals and payments"
           />
         </div>
-        <HeroComponent />
+        <HeroComponent
+          img={HomeImg}
+          heading={
+            <>
+              <p>The unseen of spending three years at Pixelgrade</p>
+            </>
+          }
+        />
+        <div className="grid grid-cols-1 w-full py-8 sm:px-2 lg:px-12 md:px-4 sm:grid-cols-1 md:grid-cols-2 bg-gray-200 rounded-lg">
+          <div className="flex flex-col gap-4 justify-center sm:pl-4 ">
+            <h1 className="text-2xl sm:text-2xl md:text-4xl lg:text-4xl font-semibold text-[#4D4D4D] text-balance text-center sm:text-left">
+              Helping a local <br />
+              <span className="text-[#43A046]">Business reinvent itself</span>
+            </h1>
+
+            <h2 className="text-[#263238] text-base text-balance text-center sm:text-left">
+              We reach here with out hardwork and dedication
+            </h2>
+          </div>
+          <div className="px-4 md:px-8 py-6 space-y-6 md:space-y-0 md:space-x-5">
+            <div className="flex flex-col md:flex-row gap-6 md:gap-5 justify-start md:justify-center">
+              <div className="flex gap-3 items-center group hover:bg-gray-50 p-2 rounded-lg transition-all duration-300">
+                <img src={Icon1} className="h-7 w-7 object-contain" />
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-[#4D4D4D] text-wrap break-words">
+                  2,245,341 <br />
+                  <span className="text-[#4D4D4D] text-sm font-normal block md:inline-block border-b border-transparent group-hover:border-gray-300">
+                    Members
+                  </span>
+                </h2>
+              </div>
+
+              <div className="flex gap-3 items-center group hover:bg-gray-50 p-2 rounded-lg transition-all duration-300">
+                <img src={Icon2} className="h-10 w-10 object-contain" />
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-[#4D4D4D] text-wrap break-words">
+                  46,328 <br />
+                  <span className="text-[#4D4D4D] text-sm font-normal block md:inline-block border-b border-transparent group-hover:border-gray-300">
+                    Clubs
+                  </span>
+                </h2>
+              </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-6 md:gap-5 justify-start md:justify-center">
+              <div className="flex gap-3 items-center group hover:bg-gray-50 p-2 rounded-lg transition-all duration-300">
+                <img src={Icon3} className="h-7 w-7 object-contain" />
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-[#4D4D4D] text-wrap break-words">
+                  828,867 <br />
+                  <span className="text-[#4D4D4D] text-sm font-normal block md:inline-block border-b border-transparent group-hover:border-gray-300">
+                    Event Bookings
+                  </span>
+                </h2>
+              </div>
+
+              <div className="flex gap-3 items-center group hover:bg-gray-50 p-2 rounded-lg transition-all duration-300">
+                <img src={Icon4} className="h-6 w-6 object-contain" />
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-[#4D4D4D] text-wrap break-words">
+                  1,926,436 <br />
+                  <span className="text-[#4D4D4D] text-sm font-normal block md:inline-block border-b border-transparent group-hover:border-gray-300">
+                    Payments
+                  </span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <HeroComponent
+          img={Pana}
+          heading={<p>How to design your site footer like we did</p>}
+          description={
+            <p>
+              Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor,
+              augue nec tincidunt molestie, massa nunc varius arcu, at
+              scelerisque elit erat a magna. Donec quis erat at libero ultrices
+              mollis. In hac habitasse platea dictumst. Vivamus vehicula leo
+              dui, at porta nisi facilisis finibus. In euismod augue vitae nisi
+              ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla
+              commodo faucibus efficitur quis massa. Praesent felis est, finibus
+              et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus
+              ipsum id gravida.
+            </p>
+          }
+        />
       </div>
     </div>
   );
